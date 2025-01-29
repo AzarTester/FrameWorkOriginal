7493f4d3327244a7e0cc3f6f05d36557e718aa41
unknown <azarn72@gmail.com>
2025-01-29 12:21:09 +0530

    First Commit

---
+++ 14SetAutomation_Framework/src/test/java/testCases/TC08_HomePageLogo.java
@@ -0,0 +1,17 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+import testBase.BaseClass;
+
+public class TC08_HomePageLogo extends BaseClass
+{
+	
+	@Test
+	public void Methofname() 
+	{
+		
+		
+	}
+
+}
