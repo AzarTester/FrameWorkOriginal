18b4c78ce18cb382fa490d6cd25005d0ee69dad3
unknown <azarn72@gmail.com>
2025-01-30 16:03:30 +0530

    updated file

--- 14SetAutomation_Framework/src/test/java/PageObjects/GitTest.java
+++ 14SetAutomation_Framework/src/test/java/PageObjects/GitTest.java
@@ -3,6 +3,7 @@ package PageObjects;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("This is to test git commit");
+		System.out.println("awdawdawkdha owdha wodha");
 	}
 
 }
