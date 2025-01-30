bc884c5b7ff2fb4127a554b79568decde6430452
unknown <azarn72@gmail.com>
2025-01-30 16:23:08 +0530

    deleted POM class

--- 14SetAutomation_Framework/src/test/java/PageObjects/GitTest.java
+++
@@ -1,9 +0,0 @@
-package PageObjects;
-
-public class GitTest {
-	public static void main(String[] args) {
-		System.out.println("This is to test git commit");
-		System.out.println("awdawdawkdha owdha wodha");
-	}
-
-}
